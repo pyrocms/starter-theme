fbc30303b8f033fc3336268f9986b3563bd7ebea
Ryan Thompson <ryan@pyrocms.com>
2016-02-21 05:52:45 -0600

    Initial commit

---
+++ src/StarterTheme.php
@@ -0,0 +1,16 @@
+<?php namespace Pyrocms\StarterTheme;
+
+use Anomaly\Streams\Platform\Addon\Theme\Theme;
+
+/**
+ * Class StarterTheme
+ *
+ * @link          http://anomaly.is/streams-platform
+ * @author        AnomalyLabs, Inc. <hello@anomaly.is>
+ * @author        Ryan Thompson <ryan@anomaly.is>
+ * @package       Pyrocms\StarterTheme
+ */
+class StarterTheme extends Theme
+{
+
+}
