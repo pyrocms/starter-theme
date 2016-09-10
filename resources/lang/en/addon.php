394ff0e62a87231fa12a3d6e8bd6bde3c2c5b958
aladinfajr <aladinfajr@gmail.com>
2016-09-10 22:23:53 +0300

    translate starter theme to arabic

--- resources/lang/en/addon.php
+++ resources/lang/en/addon.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'title'       => 'Starter',
-    'name'        => 'Starter Theme',
-    'description' => 'A starter theme based on the Freelance Theme from http://startbootstrap.com'
+    'title'       => 'البداية',
+    'name'        => 'سمة البداية',
+    'description' => 'سمة البداية تعتمد على سمة مجانية على http://startbootstrap.com'
 ];
